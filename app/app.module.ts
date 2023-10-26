7c0d122c82ef0f69239d8c810ddd103fa188f37d
yuhyeon99 <jujini31@gmail.com>
2023-10-26 22:32:10 +0900

    어플리케이션 컴포넌트 및 모듈 정의

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { AppComponent } from "./app.component";
+import ApplicationComponent from "./components/application/application.component";
+import CarouselComponent from "./components/carousel/carousel.component";
+import FooterComponent from "./components/footer/footer.component";
+import NavbarComponent from "./components/navbar/navbar.component";
+import ProductItemComponent from "./components/product-item/product-item.component";
+import SearchComponent from "./components/search/search.component";
+import StarsComponent from "./components/stars/stars.component";
+import { ProductService } from "./services/product.service";
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent],
+  declarations: [
+    ApplicationComponent,
+    CarouselComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductItemComponent,
+    SearchComponent,
+    StarsComponent,
+  ],
+  providers:[ProductService]
+  bootstrap: [ApplicationComponent],
 })
 export class AppModule {}
